35aa75f242b647d9a95df17b8e88727a0dbcd293
TJ Young <tyoung@macu.com>
2014-03-21 13:38:26 -0600

    Added target tracking

--- src/KillerRobot/TankKiller.cs
+++ src/KillerRobot/TankKiller.cs
@@ -1,4 +1,5 @@
-﻿using Robocode;
+﻿using System.Reflection;
+using Robocode;
 using Robocode.Util;
 using System;
 using System.Drawing;
@@ -12,6 +13,8 @@ namespace TJY
         private double _direction;
         private double _deathCount;
         private Random _randomGenerator;
+        private string _target;
+        private int _searchTimeout;
 
         #endregion Member Variables
 
@@ -20,6 +23,8 @@ namespace TJY
         public override void Run()
         {
             BulletColor = (Color.Red);
+            _target = "";
+            _searchTimeout = 0;
             this._randomGenerator = new Random();
             this._direction = double.PositiveInfinity;
             SetTurnRadarRightRadians(this._direction);
@@ -27,36 +32,61 @@ namespace TJY
 
         public override void OnScannedRobot(ScannedRobotEvent evnt)
         {
-            var index = 30;
-            int matchPosition;
-
-            SetAhead(this._direction *= (0.92 - (this._deathCount > 1.25 ? this._randomGenerator.Next() : (Math.Round(_deathCount) - (this.getTime() % 13)))));
-
-            var absoluteBearing = evnt.BearingRadians + HeadingRadians;
-            enemyHistory = string.Concat((char)(evnt.Velocity * (Math.Sin(evnt.HeadingRadians - (absoluteBearing)))), enemyHistory);
-
-            while (true)
+            //if this is not our target return immediately
+            if (!string.Equals("", _target) && !string.Equals(evnt.Name, _target))
             {
-                matchPosition = enemyHistory.IndexOf(enemyHistory.Substring(0, index--), 64);
-                if (matchPosition >= 0)
+                if (_searchTimeout > 10)
                 {
-                    break;
+                    _target = "";
+                    _searchTimeout = 0;
+                }
+                else
+                {
+                    _searchTimeout++;
+                    return;                    
                 }
             }
 
-            index = (int)evnt.Distance;
-            SetTurnRightRadians(Math.Cos(evnt.BearingRadians) - ((index - 250) * Velocity / 3200));
-
-            do
+            //we have a target;
+            _target = evnt.Name;
+            _searchTimeout = 0;
+            try
             {
-                absoluteBearing += (((short)enemyHistory[--matchPosition]) / evnt.Distance);
-            } while ((index -= 13) > 0);
+                var index = 30;
+                int matchPosition;
 
-            SetTurnGunRightRadians(Utils.NormalRelativeAngle(absoluteBearing - GunHeadingRadians));
-            //don't ask... Math is complicated
-            SetFire(2.333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333);
+                SetAhead(this._direction *= (0.92 - (this._deathCount > 1.25 ? this._randomGenerator.Next() : (Math.Round(_deathCount) - (this.getTime() % 13)))));
 
-            SetTurnRadarLeftRadians(RadarTurnRemainingRadians);
+                var absoluteBearing = evnt.BearingRadians + HeadingRadians;
+                enemyHistory = string.Concat((char)(evnt.Velocity * (Math.Sin(evnt.HeadingRadians - (absoluteBearing)))), enemyHistory);
+
+                while (true)
+                {
+                    matchPosition = enemyHistory.IndexOf(enemyHistory.Substring(0, index--), 64);
+                    if (matchPosition >= 0)
+                    {
+                        break;
+                    }
+                }
+
+                index = (int)evnt.Distance;
+                SetTurnRightRadians(Math.Cos(evnt.BearingRadians) - ((index - 250) * Velocity / 3200));
+
+                do
+                {
+                    absoluteBearing += (((short)enemyHistory[--matchPosition]) / evnt.Distance);
+                } while ((index -= 13) > 0);
+
+                SetTurnGunRightRadians(Utils.NormalRelativeAngle(absoluteBearing - GunHeadingRadians));
+                //don't ask... Math is complicated
+                SetFire(2.333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333);
+
+                SetTurnRadarLeftRadians(RadarTurnRemainingRadians);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public override void OnDeath(DeathEvent evnt)
@@ -66,7 +96,15 @@ namespace TJY
 
         public override void OnHitWall(HitWallEvent evnt)
         {
-            this._direction *= -1.0;
+            this._direction = (this._direction + 45) * -1;
+        }
+
+        public override void OnHitByBullet(HitByBulletEvent evnt)
+        {
+            if (!string.Equals(_target, evnt.Name))
+            {
+                _target = evnt.Name;
+            }
         }
 
         #endregion AdvancedRobot Members
